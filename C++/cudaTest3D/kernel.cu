1b16b11280c5907ffe28a03702c5521f83663f0f
Santi <santiagoj.inbox@gmail.com>
2023-04-23 14:02:57 -0700

    commenting

--- C++/cudaTest3D/kernel.cu
+++ C++/cudaTest3D/kernel.cu
@@ -4,10 +4,11 @@
 #include <inttypes.h>
 #include <chrono>
 
-#define M_PI 3.14159265359
+#define M_PI 3.14159265359 // pi constant
+#define DEGTORAD M_PI/180 // conversion factor from degrees to radians
+#define N 1000 // number of points
 
-#define N 1000
-
+// function used for stopwatch
 uint64_t timeSinceEpochMillisec() {
   using namespace std::chrono;
   return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
@@ -20,11 +21,12 @@ class Point {
         float z = 0;
 };
 
+// rotation vectors
+float yaw = 45 * DEGTORAD;
+float pitch = 0 * DEGTORAD;
+float roll = 0 * DEGTORAD;
 
-float yaw = 45*(M_PI/180);
-float pitch = 0*(M_PI/180);
-float roll = 0*(M_PI/180);
-
+// pre-calculate matrix constants - ultimately this would be run once a frame
 float a = cos(yaw)*cos(pitch);
 float b = cos(yaw)*sin(pitch)*sin(roll)-sin(yaw)*cos(roll);
 float c = cos(yaw)*sin(pitch)*cos(roll)+sin(yaw)*sin(roll);
@@ -35,66 +37,54 @@ float g = -sin(pitch);
 float h = cos(pitch)*sin(roll);
 float i = cos(pitch)*cos(roll);
 
-
+// kernel to multiply by the matrix constants
 __global__ void MatrixMultiply(Point *points, float *matrix, Point *newpoints, int numPoints) {
-
+    // calculate index
     int k = threadIdx.x + blockIdx.x * blockDim.x;
 
+    // controls that kernel will only work for number of points
     if (k < numPoints) {
+        // retrieves vector from GPU memory
         int x = points[k].x;
         int y = points[k].y;
         int z = points[k].z;
 
-        
+        // multiply vector by matrix
         newpoints[k].x = x*matrix[0]+y*matrix[1]+z*matrix[2];
         newpoints[k].y = x*matrix[3]+y*matrix[4]+z*matrix[5];
         newpoints[k].z = x*matrix[6]+y*matrix[7]+z*matrix[8];
     }
-
-
-
 }
-
 
 int main() {
 
+    size_t arraySize = N * sizeof(Point); // compute size of point array
 
-    // compute size of point array
-    int numPoints = 10000000;
-    size_t arraySize = numPoints * sizeof(Point);
     // allocate point arrays
     Point *h_points = (Point *)malloc(arraySize);
     Point *h_newpoints = (Point *)malloc(arraySize);
 
+    size_t matrixSize = 9 * sizeof(float); // compute size of matrix
 
-    // compute size of matrix
-    size_t matrixSize = 9 * sizeof(float);
-    // allocate matrix values
-    float *h_matrix = (float *)malloc(matrixSize);
+    float *h_matrix = (float *)malloc(matrixSize); // allocate matrix values
 
     // check allocations
     if (h_points == NULL || h_matrix == NULL || h_newpoints == NULL) {
-        printf("Failed to allocate host vectors!\n");
+        printf("Failed to allocate vectors on host\n");
         return 1;
     }
 
     // initialize points
-    for (int i = 0; i < numPoints; i++) {
+    for (int i = 0; i < N; i++) {
         h_points[i].x = i;
         h_points[i].y = i;
         h_points[i].z = i;
     }
 
     // initialize matrix
-    h_matrix[0] = a;
-    h_matrix[1] = b;
-    h_matrix[2] = c;
-    h_matrix[3] = d;
-    h_matrix[4] = e;
-    h_matrix[5] = f;
-    h_matrix[6] = g;
-    h_matrix[7] = h;
-    h_matrix[8] = i;
+    h_matrix[0] = a; h_matrix[1] = b; h_matrix[2] = c;
+    h_matrix[3] = d; h_matrix[4] = e; h_matrix[5] = f;
+    h_matrix[6] = g; h_matrix[7] = h; h_matrix[8] = i;
 
     // allocate memory on device
     Point *d_points = NULL;
@@ -115,16 +105,8 @@ int main() {
     // run kernel
     MatrixMultiply<<<blocks, threadsPerBlock>>>(d_points, d_matrix, d_newpoints, arraySize);
 
-    uint64_t zero = timeSinceEpochMillisec();
-
     // copy results from device to host
     cudaMemcpy(h_newpoints, d_newpoints, arraySize, cudaMemcpyDeviceToHost);
-
-    printf("%" PRIu64 "ms\n", timeSinceEpochMillisec() - zero);
-
-    /*for (int i = 0; i < 300; i++) {
-        printf("%f : %f\n", h_points[i].x, round(h_newpoints[i].x));
-    }*/
 
     // free device memory
     cudaFree(d_points);
@@ -141,34 +123,3 @@ int main() {
 
     return 0;
 }
-
-
-
-/*
-
-
-
-__global__ void MatrixMultiply(Point *p) {
-
-    int k = threadIdx.x;
-
-    int x = p[k].x;
-    int y = p[k].y;
-    int z = p[k].z;
-
-    p[k].x = x*a+y*b+z*c;
-    p[k].y = x*d+y*e+f*z;
-    p[k].z = x*g+y*h+i*z;
-
-
-
-for (int i = 0; i < N; i++) {
-        points[i].x = 2;
-        points[i].y = 2;
-        points[i].z = 2; 
-    }
-
-
-}
-
-*/
